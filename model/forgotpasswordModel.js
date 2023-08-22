963cf85b2c63c0b8ec3757f181f64a17ba7013d6
shubhamadav <shubhamyad100@gmail.com>
2023-08-23 01:25:01 +0530

    forgotpassword

---
+++ model/forgotpasswordModel.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+const sequelize =require('../util/database')
+
+const ForgotPassword = sequelize.define('forgotpassword_tb', {
+    id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    active: Sequelize.BOOLEAN,  
+});
+
+module.exports = ForgotPassword;
